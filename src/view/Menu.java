a58c8c8db50d95787c775e535cead16e69722533
josepmartorell <martorelljosep@gmail.com>
2022-05-08 16:20:18 +0200

    main frame

---
+++ src/view/Menu.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jtech
+ */
+public class Menu extends JFrame{
+
+    public Menu() {
+        setSize(1200, 900);
+        setVisible(true);
+    }
+    
+   
+}
